dd8c1aa2a70bc9b5a9c09f5d54fbb5b5d2824cb7
Tatsuya Hirota <h1208tatsuya@gmail.com>
2018-02-01 00:55:39 +0900

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
 import 'uikit/dist/css/uikit.css'
 import './theme/uikit.app-theme.scss'
-UIkit.use(Icons);
+UIkit.use(Icons)
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
